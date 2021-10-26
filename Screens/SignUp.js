a8a53227d2fcdb65e16847049b28b1680747459e
InnovativeCoder <aashitsingh2010@gmail.com>
2021-10-26 18:52:57 -0400

    sanitized all the inputs

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,11 +1,99 @@
+import { Field, Formik } from 'formik';
 import { default as React } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as yup from 'yup';
+import CustomInput from '../components/customInput';
 
-function SignUp() {
+
+function SignUp({navigation}) {
+
+    const signUpValidationSchema = yup.object().shape({
+        fullName: yup
+          .string()
+          .matches(/(\w.+\s).+/, 'Enter at least 2 names')
+          .required('Full name is required'),
+        phoneNumber: yup
+          .string()
+          .matches(/(01)(\d){8}\b/, 'Enter a valid phone number')
+          .required('Phone number is required'),
+        email: yup
+          .string()
+          .email("Please enter valid email")
+          .required('Email is required'),
+        password: yup
+          .string()
+          .matches(/\w*[a-z]\w*/,  "Password must have a small letter")
+          .matches(/\w*[A-Z]\w*/,  "Password must have a capital letter")
+          .matches(/\d/, "Password must have a number")
+          .matches(/[!@#$%^&*()\-_"=+{}; :,<.>]/, "Password must have a special character")
+          .min(8, ({ min }) => `Password must be at least ${min} characters`)
+          .required('Password is required'),
+        confirmPassword: yup
+          .string()
+          .oneOf([yup.ref('password')], 'Passwords do not match')
+          .required('Confirm password is required'),
+      })
+      
     return (
         <View style={styles.container}>
-          <Text>Register a new account</Text>
+          <Text>Register</Text>
           {/* <StatusBar style="auto" /> */}
+          <Formik
+            validationSchema={signUpValidationSchema}
+            initialValues={{
+              fullName: '',
+              email: '',
+              phoneNumber: '',
+              password: '',
+              confirmPassword: '',
+            }}
+            onSubmit={values => console.log(values)}
+          >
+            {({ handleSubmit, isValid }) => (
+              <>
+                <Field
+                  component={CustomInput}
+                  name="fullName"
+                  placeholder="Full Name"
+                />
+                <Field
+                  component={CustomInput}
+                  name="email"
+                  placeholder="Email Address"
+                  keyboardType="email-address"
+                />
+                <Field
+                  component={CustomInput}
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  keyboardType="numeric"
+                />
+                <Field
+                  component={CustomInput}
+                  name="passowrd"
+                  placeholder="Password"
+                  secureTextEntry
+                />
+                <Field
+                  component={CustomInput}
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  secureTextEntry
+                />
+
+                <Button
+                  onPress={handleSubmit}
+                  title="SIGN UP"
+                  disabled={!isValid}
+                />
+              </>
+            )}
+          </Formik>
+          <Button
+                title="Already Registed?"
+                onPress={() => navigation.navigate('Login')}
+                style={styles.registerButton}
+            />
         </View>
     )
 }
@@ -14,10 +102,21 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
+    //   alignItems: 'center',
       justifyContent: 'center',
+      padding: 20,
     },
+    input: {
+        borderBottomColor: '#000',
+        borderBottomWidth: 2,
+        margin: 10,
+        padding: 0,
+    },
+    registerButton: {
+        margin: 10,
+        color: '#000',
+        backgroundColor: '#fff',
+    }
   });
 
 export default SignUp
-
---
+++ components/customInput.js
@@ -0,0 +1,54 @@
+// CustomInput.js
+import React from 'react'
+import { StyleSheet, Text, TextInput } from 'react-native'
+
+const CustomInput = (props) => {
+  const {
+    field: { name, onBlur, onChange, value },
+    form: { errors, touched, setFieldTouched },
+    ...inputProps
+  } = props
+
+  const hasError = errors[name] && touched[name]
+
+  return (
+    <>
+      <TextInput
+        style={[
+          styles.textInput,
+          hasError && styles.errorInput
+        ]}
+        value={value}
+        onChangeText={(text) => onChange(name)(text)}
+        onBlur={() => {
+          setFieldTouched(name)
+          onBlur(name)
+        }}
+        {...inputProps}
+      />
+      {hasError && <Text style={styles.errorText}>{errors[name]}</Text>}
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    width: '100%',
+    margin: 5,
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 3,
+    padding: 10,
+  },
+  errorText: {
+    fontSize: 10,
+    color: 'red',
+  },
+  errorInput: {
+    borderColor: 'red',
+  }
+})
+
+export default CustomInput
