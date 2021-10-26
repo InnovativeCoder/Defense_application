22142ff83cb290bb8cced7736520be2c828fe563
InnovativeCoder <aashitsingh2010@gmail.com>
2021-10-26 17:07:59 -0400

    Added formik

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Formik } from 'formik';
+import { default as React } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 function Login({navigation}) {
     return (
         <View style={styles.container}>
           <Text>Login</Text>
           {/* <StatusBar style="auto" /> */}
+          <Formik
+            initialValues={{ email: '' }}
+            onSubmit={values => console.log(values)}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View>
+                <TextInput
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                style={styles.input}
+                />
+                <Button onPress={handleSubmit} title="Submit" />
+            </View>
+            )}
+        </Formik>
           <Button
                 title="Register?"
                 onPress={() => navigation.navigate('SignUp')}
@@ -21,6 +38,11 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input: {
+        borderBottomColor: '#000',
+        borderBottomWidth: 2,
+        margin: 10,
+    }
   });
 
 export default Login
